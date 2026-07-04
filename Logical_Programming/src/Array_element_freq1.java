f3d1447cd71bb7e151fe80ae3527182b65e1be56
Ksagar29 <isagargk@gmail.com>
2026-07-04 14:57:14 +0530

    Fix class declaration formatting in Array_element_freq1

--- Logical_Programming/src/Array_element_freq1.java
+++ Logical_Programming/src/Array_element_freq1.java
@@ -4,7 +4,7 @@ import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
-public class Array_element_freq1 {
+public class Array_element_freq1 {  
 
 	public static void main(String[] args) {
 
